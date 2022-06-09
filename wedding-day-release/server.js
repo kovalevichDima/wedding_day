b4f424a5834c05ef5b2ea5b464820a482f698918
kovalevichdDima <kovalevichdima16@gmail.com>
2022-06-10 02:05:47 +0300

    wedding for Sanya

--- wedding-day-release/server.js
+++ wedding-day-release/server.js
@@ -5,9 +5,6 @@ const app = express();
 
 app.use(express.static(`${__dirname}/src`));
 
-app.use("/", proxy("http://localhost:81"));
-//app.use("/", proxy("http://kzz.by:81"));
-
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
